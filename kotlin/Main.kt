2235255a4baff8f7363379cce48f699f2cb0db04
SoldDA <146986108+SoldDA@users.noreply.github.com>
2024-09-22 16:23:21 +0300

    Update Main.kt

--- kotlin/Main.kt
+++ kotlin/Main.kt
@@ -1,8 +1,7 @@
 import main.kotlin.Student
 
 fun main() {
-    val student1 = Student(
-        1,
+    val Danil = Student(
         "Спиридонов",
         "Данил",
         "Александрович",
@@ -12,30 +11,15 @@ fun main() {
         "https://github.com/SolDA"
     )
 
-    val student2 = Student(
-        2,
+    val Zhenya = Student(
         "Прокопенко",
         "Евгений",
-        "Константинович",
-        null,
-        "@OutcustBur",
-        null,
-         null
-    )
-
-    val student3 = Student.createStudent(
-        "ID" to 3,
-        "Фамилия" to "Шестак",
-        "Имя" to "Виктория",
-        "Отчество" to "Александровна",
-        "Телефон" to "+79833256666",
-        "GitHub" to "https://vikya_f"
+        "Константинович"
     )
 
     println("Информация о студентах:")
-    println(student1.toString())
-    println(student2.toString())
-    println(student3.toString())
-    println("\nВалидация студента1: ${student1.validate()}")
-    println("\nВалидация студента2: ${student2.validate()}")
+    println(Danil.toString())
+    println(Zhenya.toString())
+    Zhenya.setContacts("12345678910", "@kashpo", "kashpo@mail.ru")
+    println(Zhenya.toString())
 }
